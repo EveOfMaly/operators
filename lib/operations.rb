085c0dc9535b6820f258110154c933b333974fc9
EveOfMaly <john.a.kyeremeh@gmail.com>
2021-08-28 06:27:33 -0700

    commit tree c0a6cf2a1d765a9850df306b01e208c75902a526

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
+require 'pry'
 def unsafe?(speed)
-
+    #returns true if the speed is either below 40 or above 60 
+    if speed < 40 || speed > 60 
+         true 
+    else
+        false
+    end
 end
 
 
 
 def not_safe?(speed)
-	
+    #returns true if the speed is either below 40 or above 60 using a ternary operator condition ? action_if_true : action_if_fase
+	speed < 40 || speed > 60 ? true : false
 end
-	
+
+
+
 
 
